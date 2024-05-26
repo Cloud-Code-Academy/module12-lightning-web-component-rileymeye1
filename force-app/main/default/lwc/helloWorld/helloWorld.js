76716c787260feeb27982ce7e668f39b619e16d6
Riley Meyer <134163111+rileymeye1@users.noreply.github.com>
2024-05-25 23:36:28 -0500

    Video 1 Complete

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+
+  greeting = 'Everyone';
+
+  changeHandler(event) {
+    console.log(event.target.value);
+    this.greeting = event.target.value;
+  }
+}
